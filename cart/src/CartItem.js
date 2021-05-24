286ef8082f940f09809bcf4351c14b4577b059ce
Bilal <b.umrani75@gmail.com>
2021-05-25 00:08:37 +0500

    we have to use setState function of React.Component to re render our component

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -14,7 +14,34 @@ class CartItem extends React.Component {
     }
 
   increaseQty = () =>{
-        console.log(this.state)
+        // if we directly increse the value of qty the value of qty in object will be incresed but react will dont know about it and it will not re render it so for that we use setState() function that is in React.Component, there are two ways to use it. following are the ways:
+
+        // this.setState({
+
+        //     qty: this.state.qty+=1
+        // }) // in this way we give it an object and write the changes in state
+
+
+        // the other way is: by passing callback functiont to the setState() lets see
+        this.setState((preState)=>{
+                return {
+                    qty: preState.qty+=1
+                }
+        })
+
+        // calling the setState function basically caouse the re rendering of our component
+
+    }
+
+
+    decreseQty = () =>{
+
+        this.setState((preState)=>{
+
+            return {
+                qty: preState.qty-=1
+            }
+        })
     }
     
     render()
@@ -34,7 +61,7 @@ class CartItem extends React.Component {
                     <div>{qty}</div>
                     <div className='cart-item-action'>
                     <button onClick={this.increaseQty}>+</button>
-                    <button>-</button>
+                    <button onClick={this.decreseQty}>-</button>
                     <button>delete</button>
                     </div>
                 </div>
